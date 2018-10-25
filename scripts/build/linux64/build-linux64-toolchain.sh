24400f4e2a09412e58330e94aeb31c28eb953ae5
Robin Jones <robin@networkfusion.co.uk>
2018-10-25 11:18:16 +0100

    Correct GCC URL Update binutils

--- scripts/build/linux64/build-linux64-toolchain.sh
+++ scripts/build/linux64/build-linux64-toolchain.sh
@@ -12,8 +12,8 @@ set -eu
 # 'LICENSE', which is part of this source code package.
 #
 
-BINUTILS="https://ftp.gnu.org/gnu/binutils/binutils-2.30.tar.bz2"
-GCC="https://ftp.gnu.org/gnu/gcc/gcc-7.3.0/gcc-8.1.0.tar.gz"
+BINUTILS="https://ftp.gnu.org/gnu/binutils/binutils-2.31.1.tar.bz2"
+GCC="https://ftp.gnu.org/gnu/gcc/gcc-8.1.0/gcc-8.1.0.tar.gz"
 MAKE="https://ftp.gnu.org/gnu/make/make-4.2.1.tar.bz2"
 NEWLIB="ftp://sourceware.org/pub/newlib/newlib-3.0.0.20180226.tar.gz"
 
