d0cf892fe8b286cf17e2e209d2bb50b299f18b96
networkfusion <robin+github@networkfusion.co.uk>
2018-11-07 16:56:12 +0000

    update to gcc 8.2

--- scripts/build/linux64/build-linux64-toolchain.sh
+++ scripts/build/linux64/build-linux64-toolchain.sh
@@ -13,7 +13,7 @@ set -eu
 #
 
 BINUTILS="https://ftp.gnu.org/gnu/binutils/binutils-2.31.1.tar.bz2"
-GCC="https://ftp.gnu.org/gnu/gcc/gcc-8.1.0/gcc-8.1.0.tar.gz"
+GCC="https://ftp.gnu.org/gnu/gcc/gcc-8.2.0/gcc-8.2.0.tar.gz"
 MAKE="https://ftp.gnu.org/gnu/make/make-4.2.1.tar.bz2"
 NEWLIB="ftp://sourceware.org/pub/newlib/newlib-3.0.0.20180226.tar.gz"
 
