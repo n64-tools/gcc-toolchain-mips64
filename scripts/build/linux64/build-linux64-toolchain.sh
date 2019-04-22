d4977a22dd2ea0e27d9da8acfb9c65beedceaf10
Robin Jones <networkfusion@users.noreply.github.com>
2019-04-22 11:38:30 +0100

    update linux buildchain to latest release

--- scripts/build/linux64/build-linux64-toolchain.sh
+++ scripts/build/linux64/build-linux64-toolchain.sh
@@ -12,10 +12,10 @@ set -eu
 # 'LICENSE', which is part of this source code package.
 #
 
-BINUTILS="https://ftp.gnu.org/gnu/binutils/binutils-2.31.1.tar.bz2"
-GCC="https://ftp.gnu.org/gnu/gcc/gcc-8.2.0/gcc-8.2.0.tar.gz"
+BINUTILS="https://ftp.gnu.org/gnu/binutils/binutils-2.32.tar.bz2"
+GCC="https://ftp.gnu.org/gnu/gcc/gcc-8.3.0/gcc-8.3.0.tar.gz"
 MAKE="https://ftp.gnu.org/gnu/make/make-4.2.1.tar.bz2"
-NEWLIB="ftp://sourceware.org/pub/newlib/newlib-3.0.0.20180226.tar.gz"
+NEWLIB="ftp://sourceware.org/pub/newlib/newlib-3.1.0.tar.gz"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${SCRIPT_DIR} && mkdir -p {stamps,tarballs}
