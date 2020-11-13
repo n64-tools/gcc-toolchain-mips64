dc1be396522b158d3ffc4b107be5800e3e276e05
Robin Jones <networkfusion@users.noreply.github.com>
2020-11-13 03:12:05 +0000

    Update pipeline image (#9)
    * Updates the pipeline to use the latest LTS version of ubuntu (currently 18.04).
    * Updates Azure CLI package
    * Updates initial GCC version to 10.
    * Updates initial G++ version to 10.
    * Updates linux mips64 Make version to 4.3.
    * Patches Windows mips64 Make to 4.2.1 with patch (due to issue with 4.2.1 and incompatibility with 4.3 with mingw).
    * Updates comments for easier understanding when building locally.

--- scripts/build/linux64/build-linux64-toolchain.sh
+++ scripts/build/linux64/build-linux64-toolchain.sh
@@ -14,7 +14,7 @@ set -eu
 
 BINUTILS="https://ftp.gnu.org/gnu/binutils/binutils-2.35.tar.bz2"
 GCC="https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.gz"
-MAKE="https://ftp.gnu.org/gnu/make/make-4.2.1.tar.gz"
+MAKE="https://ftp.gnu.org/gnu/make/make-4.3.tar.gz"
 NEWLIB="https://sourceware.org/pub/newlib/newlib-3.3.0.tar.gz"
 GDB="https://ftp.gnu.org/gnu/gdb/gdb-10.1.tar.gz"
 
