db457977e8359c6e34765d1e6bd20f56e9009681
Robin Jones <robin@networkfusion.co.uk>
2022-04-13 02:44:07 +0100

    Revert dependencies to work out issue...

--- scripts/build/win64/build-win64-toolchain.sh
+++ scripts/build/win64/build-win64-toolchain.sh
@@ -12,13 +12,13 @@ set -eu
 # 'LICENSE', which is part of this source code package.
 #
 
-BINUTILS="https://ftp.gnu.org/gnu/binutils/binutils-2.38.tar.gz"
-GCC="https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.gz" #can we move to 10.3?
-GMP="https://ftp.gnu.org/gnu/gmp/gmp-6.2.1.tar.xz" # No gz file available1
+BINUTILS="https://ftp.gnu.org/gnu/binutils/binutils-2.35.1.tar.gz" # can we move to 2.38?
+GCC="https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.gz" # can we move to 10.3?
+GMP="https://ftp.gnu.org/gnu/gmp/gmp-6.2.1.tar.xz" # No gz file available!
 MAKE="https://ftp.gnu.org/gnu/make/make-4.2.1.tar.gz"
 MPC="https://ftp.gnu.org/gnu/mpc/mpc-1.2.1.tar.gz"
 MPFR="https://ftp.gnu.org/gnu/mpfr/mpfr-4.1.0.tar.gz"
-NEWLIB="https://sourceware.org/pub/newlib/newlib-4.1.0.tar.gz"
+NEWLIB="https://sourceware.org/pub/newlib/newlib-3.3.0.tar.gz" # can we move to 4.1.0?
 GDB="https://ftp.gnu.org/gnu/gdb/gdb-10.1.tar.gz"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
