90cdb6c8e76edb2bfb96cab0c64a2d93ca6ff12c
Robin Jones <robin@networkfusion.co.uk>
2022-04-21 19:21:28 +0100

    Revert GDB version update

--- scripts/build/linux64/build-linux64-toolchain.sh
+++ scripts/build/linux64/build-linux64-toolchain.sh
@@ -19,7 +19,7 @@ BUILD_NUM_JOBS="--jobs=$NUM_CPU_THREADS --load-average=$NUM_CPU_THREADS"
 BINUTILS="https://ftp.gnu.org/gnu/binutils/binutils-2.38.tar.gz"
 GCC="https://ftp.gnu.org/gnu/gcc/gcc-11.3.0/gcc-11.3.0.tar.gz"
 NEWLIB="https://sourceware.org/pub/newlib/newlib-4.1.0.tar.gz"
-GDB="https://ftp.gnu.org/gnu/gdb/gdb-11.2.tar.gz"
+GDB="https://ftp.gnu.org/gnu/gdb/gdb-10.2.tar.gz" # fails to x compile on 11.2. requires investigation!
 
 BUILD=${BUILD:-x86_64-linux-gnu}
 HOST=${HOST:-x86_64-linux-gnu}
