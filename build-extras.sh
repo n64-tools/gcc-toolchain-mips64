00f49c7ce3afd8747918f4a6b40fcc977b7e871a
Robin Jones <networkfusion@users.noreply.github.com>
2023-01-27 00:31:45 +0000

    further try

--- build-extras.sh
+++ build-extras.sh
@@ -63,13 +63,13 @@ cd "$BUILD_PATH"
 test -f "gdb-$GDB_V.tar.gz"           || download "https://ftp.gnu.org/gnu/gdb/gdb-$GDB_V.tar.gz"
 test -d "gdb-$GDB_V"                  || tar -xzf "gdb-$GDB_V.tar.gz"
 
-if [ "$GMP_V" != "" ]; then
-    test -f "gmp-$GMP_V.tar.xz"       || download "https://ftp.gnu.org/gnu/gmp/gmp-$GMP_V.tar.bz2"
-    test -d "gmp-$GMP_V"              || tar -xf "gmp-$GMP_V.tar.bz2" # note: no .gz download file currently available
-    pushd "gdb-$GDB_V"
-    ln -sf ../"gmp-$GMP_V" "gmp"
-    popd
-fi
+# if [ "$GMP_V" != "" ]; then
+#     test -f "gmp-$GMP_V.tar.xz"       || download "https://ftp.gnu.org/gnu/gmp/gmp-$GMP_V.tar.bz2"
+#     test -d "gmp-$GMP_V"              || tar -xf "gmp-$GMP_V.tar.bz2" # note: no .gz download file currently available
+#     pushd "gdb-$GDB_V"
+#     ln -sf ../"gmp-$GMP_V" "gmp"
+#     popd
+# fi
 
 if [ "$EXPAT_V" != "" ]; then
     test -f "expat-$EXPAT_V.tar.xz"   || download "https://github.com/libexpat/libexpat/releases/download/R_2_5_0/expat-$EXPAT_V.tar.gz"
